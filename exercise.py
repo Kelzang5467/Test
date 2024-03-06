1f698f6029650a824dda5e971cc076408336928b
Tobden Thapa <02230275.cst@rub.edu.btgit config --global user.email 02230275.cst@rub.edu.bt>
2024-03-06 14:17:33 +0600

    commit code

---
+++ exercise.py
@@ -0,0 +1,11 @@
+age=int(input("Enter your age: \n"))
+ask=input("Are you a Student? (yes/no): \n")
+
+if age<=12:
+    print("you are eligible ")
+
+elif age>=13 and age<18 and ask=="yes":
+    print("you are eligible")  
+
+else:
+    print("you are not eligible")
--- first_program.py
+++ first_program.py
@@ -1,1 +1,11 @@
 print ("Hello World")
+a=2
+print(a>3 and a<0)
+age=int(input("enter your age?: \n"))
+ask=input("are you a student? (yes/no): \n")
+if age<=12:
+    print("you are eligible")
+elif age>=13 and age==18 and ask=="yes":
+    print("you are eligible")
+else:
+    print("not eligible")
